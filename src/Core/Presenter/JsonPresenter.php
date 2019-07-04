8240cf12007df69046d9681fbe75b424d93c228f
Rene Penner <rene@penner.name>
2019-07-04 21:48:11 +0200

    Add JsonPresenter test

--- src/Core/Presenter/JsonPresenter.php
+++ src/Core/Presenter/JsonPresenter.php
@@ -2,13 +2,26 @@
 namespace Wambo\Demo\Core\Presenter;
 
 use JsonSerializable;
-use Slim\Http\Response;
+use Psr\Http\Message\ResponseInterface;
+use RuntimeException;
+use Slim\Http\Stream;
 
 class JsonPresenter
 {
-    public function __invoke(Response $response, JsonSerializable $viewModel): Response
+    /**
+     * @param ResponseInterface $response
+     * @param JsonSerializable $viewModel
+     * @return ResponseInterface
+     *
+     * @throws RuntimeException, JsonException
+     */
+    public function __invoke(ResponseInterface $response, JsonSerializable $viewModel): ResponseInterface
     {
-        $statusCode = $response->getStatusCode();
-        return $response->withJson($viewModel, $statusCode);
+        $json = json_encode($viewModel, JSON_THROW_ON_ERROR);
+
+        $body = new Stream(fopen('php://temp', 'r+'));
+        $body->write($json);
+
+        return $response->withBody($body);
     }
 }
---
+++ tests/Unit/Core/Presenter/JsonPresenterTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Wambo\Tests\Unit\Core\Presenter;
+
+use PHPUnit\Framework\TestCase;
+use Slim\Http\Response;
+use Wambo\Demo\Core\Presenter\JsonPresenter;
+
+class JsonPresenterTest extends TestCase
+{
+    public function testValidJsonResponse()
+    {
+        $viewModel = $this->createMock(\JsonSerializable::class);
+        $viewModel->method('jsonSerialize')->willReturn([
+            'message' => 'Hallo Test!',
+        ]);
+
+        $response = new Response();
+        $jsonPresenter = new JsonPresenter();
+
+        $newResponse = $jsonPresenter->__invoke($response, $viewModel);
+
+        $this->assertEquals('{"message":"Hallo Test!"}', $newResponse->getBody());
+    }
+}
